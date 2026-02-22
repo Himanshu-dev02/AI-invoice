ea4a13fbff9127ef140bd5a7a8acfc353169eecf
Himanshu Meshram <meshramhimanshu20@gmail.com>
2026-02-22 22:00:48 +0530

    Update MongoDB connection string placeholder

--- backend/config/Db.js
+++ backend/config/Db.js
@@ -1,6 +1,6 @@
 import mongoose, { mongo } from "mongoose";
 
 export const connectDB = async () => {
-  await mongoose.connect('mongodb+srv://meshramhimanshu20_db_user:invoice999@cluster0.olok7oo.mongodb.net/InvoiceAI')
+  await mongoose.connect('Your mongo db key')
     .then(() => { console.log('MongoDB connected')});
 }
